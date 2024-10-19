27d1e4b683463dd92261bc86c1668fe35c10bb43
Clayton Andrade <professorclaytonandrade@gmail.com>
2024-10-19 20:29:11 -0300

    Variáveis Java

---
+++ src/aula03variaveis/Variaveis.java
@@ -0,0 +1,39 @@
+package aula03variaveis;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        //Números Inteiros
+        byte num1Byte;
+        byte num2Byte = 10;
+        num1Byte = 20;
+        num1Byte = 30;
+
+        short num1Short;
+        short num2Short = 40;
+
+        int num1Int;
+        int num2Int = 50;
+
+        long num1Long;
+        long num2Long = 100L;
+
+        //Números Decimais
+        float num1Float;
+        float num2Float = 5.3f;
+
+        double num1Double;
+        double num2Double = 8.56489;
+
+        //Não Númericos
+        char letraA = 'A';
+        char letraB = 'b';
+
+        String palavra = "Olá Mundo";
+
+        //Booleanos
+        boolean verdadeiro = true;
+        boolean falso = false;
+        boolean aprovado = false;
+        aprovado = true;
+    }
+}
